de4129944e644649b411b4e34cc83fbfd82f81c6
Pratik Shrestha <pratikstha278@gmail.com>
2026-02-05 17:37:10 +0545

    login url

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -5,3 +5,25 @@ export const fetchProducts = async() => {
     if(!response.ok) throw new Error("Failed to fetch products");
     return response.json();
 };
+
+export const loginUser = async(username, password) => {
+    const response = await fetch(`{API_URL}token/`,{
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({username, password}),
+    });
+
+    if (!response.ok) throw new Error("Invalid username or password");
+
+    const data = await response.json();
+
+    localStorage.setItem("access", data.access);
+    localStorage.setItem("refresh", data.refresh);
+    return data;
+};
+
+export const logoutUser = () => {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    window.location.href = "/";
+}
