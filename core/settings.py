606b80c2ec888391f7c737a03b114ea30ee7107c
Pratik Shrestha <pratikstha278@gmail.com>
2026-02-07 13:08:54 +0545

    final setup

--- core/settings.py
+++ core/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1%z#*ss@$h%kf&pduo&w_eoaql#l-*3nah=t$(!!1zffsm)gsk'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-fallback-key-for-local-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,9 +1,14 @@
-const API_URL = "http://127.0.0.1:8000/api/";
+const API_URL = import.meta.env.VITE_APT_URL || "http://127.0.0.1:8000/api/";
 
 export const fetchProducts = async() => {
+    try {
     const response = await fetch(`${API_URL}products/`);
-    if(!response.ok) throw new Error("Failed to fetch products");
-    return response.json();
+    if(!response.ok) throw new Error("Network response was not ok");
+    return await response.json();
+    } catch (error) {
+        console.error("API Fetch Error:", error);
+        throw error;
+    }
 };
 
 export const loginUser = async(username, password) => {
